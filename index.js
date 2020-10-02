b8d33d7b545d29a14116cbce12f730cc53523ab6
Thomas Wright <twwright@gmail.com>
2020-10-02 13:46:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+function mapToNegativize(arr) {
+  const result = [];
+  arr.forEach(n => result.push(n * -1));
+  return result;
+}
+
+function mapToNoChange(arr) {
+  return arr.slice();
+}
+
+function mapToDouble(arr) {
+  const result = [];
+  arr.forEach(n => result.push(n * 2));
+  return result;
+}
+
+function mapToSquare(arr) {
+  const result = [];
+  arr.forEach(n => result.push(n * n));
+  return result;
+}
+
+function reduceToTotal(arr, start=0) {
+  let memo = start;
+  arr.forEach(x => memo = memo + x);
+  return memo;
+}
+
+function reduceToAllTrue(arr) {
+  for (const x of arr) {
+    if (!x) return false;
+  }
+  return true;
+}
+
+function reduceToAnyTrue(arr) {
+  for (const x of arr) {
+    if (x) return true;
+  }
+  return false;
+}
+
+
+
+
+
+
+
